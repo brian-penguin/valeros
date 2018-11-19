0f3679337dd0c0a41ca20ac4ec0f4b9867732934
Brian Tenggren <btenggren20@gmail.com>
2018-11-18 20:57:25 -0500

    HEY GUESS WHAT
    Goal: Show the spells on the spells page
    
    Changes:
    - slurp that file SLURP SLURP
    - add a route for "/spells"
    - add a template
    - memoize the spells! SO FAST NOW (I'm not sure that it persists between
    requests however)

--- src/clj/valeros/routes/home.clj
+++ src/clj/valeros/routes/home.clj
@@ -12,6 +12,26 @@
 (defn about-page []
   (layout/render "about.html"))
 
+(defn memoize [f]
+  (let [mem (atom {})]
+    (fn [& args]
+      (if-let [e (find @mem args)]
+        (val e)
+        (let [ret (apply f args)]
+          (swap! mem assoc args ret)
+          ret)))))
+
+(defn spells []
+  (-> "docs/spells.csv" io/resource slurp))
+
+(def spells
+  (memoize spells))
+
+(defn spell-page []
+  (layout/render
+   "spells.html" {:spells (spells)}))
+
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/about" [] (about-page))
+  (GET "/spells" [] (spell-page)))
